b0d632d3c25b9a9a0b41b8dfcd33b6c9ba7fb86f
Gaurav kumar <88134414+gaurav25042002@users.noreply.github.com>
2024-04-16 18:43:33 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -55,7 +55,7 @@ export default function Footer() {
             variant="small"
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
           >
-            &copy; {currentYear} <a href="#">Compare Craze</a>. Developed by <strong>Adarsh, Gaurav, Nikhil, Pavitra, Saurabh </strong>
+            &copy; {currentYear} <a href="#">Compare Craze</a>. Developed by <strong>Adarsh, Gaurav, Naman, Nikhil, Saurabh </strong>
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
             <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100">
