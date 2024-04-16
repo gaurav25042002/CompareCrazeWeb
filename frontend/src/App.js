1c3da0ee6e39dbe07dd3defdfccc5c76a138d80a
Gaurav kumar <88134414+gaurav25042002@users.noreply.github.com>
2024-04-16 18:38:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,26 +6,11 @@ import Home from "./Pages/Home.jsx";
 import MainPage from "./Pages/MainPage.jsx";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
-
-  // Check if user is authenticated
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setIsAuth(true);
-      // console.log("if token")
-    } else {
-      setIsAuth(false);
-      // console.log("if no token");
-    }
-  });
   return (
       <div className="main-app">
         <Router>
           <Routes>
-            {isAuth &&  (
-              <Route exact path="/mainpage" element={<MainPage />} />
-            )}
+            <Route exact path="/mainpage" element={<MainPage />} />
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signup" element={<Signup />} />
